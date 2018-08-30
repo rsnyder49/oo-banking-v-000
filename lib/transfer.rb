efffc660f0f5ef6b31c8fe70dbb24f48e5bc8b5c
Robert Snyder <r.snyder49@gmail.com>
2018-08-30 21:40:18 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.!valid?
+    if @sender.valid? == false 
       "Transaction rejected. Please check your account balance."
     else 
       
